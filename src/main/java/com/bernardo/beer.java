a1154318b51d005fec78e3472e603ab0f6ffea22
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2023-09-26 22:27:10 -0300

    refactor: translating the plugin to english

--- src/main/java/com/bernardo/beer.java
+++ src/main/java/com/bernardo/beer.java
@@ -13,7 +13,6 @@ import org.bukkit.inventory.ShapelessRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionData;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -30,7 +29,6 @@ public final class beer extends JavaPlugin implements Listener {
         beerRecipe.addIngredient(Material.POTION);
         beerRecipe.addIngredient(Material.WHEAT);
         beerRecipe.addIngredient(Material.FERMENTED_SPIDER_EYE);
-        getServer().addRecipe(beerRecipe);
     }
 
     @Override
@@ -41,8 +39,8 @@ public final class beer extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerDrink(PlayerItemConsumeEvent event) {
         ItemStack item = event.getItem();
-        if (item != null && item.getType() == Material.POTION && item.getItemMeta().hasDisplayName() &&
-                item.getItemMeta().getDisplayName().equals("Cerveja")) {
+        if (item.getType() == Material.POTION && item.getItemMeta().hasDisplayName() &&
+                item.getItemMeta().getDisplayName().equals("Beer")) {
             Player player = event.getPlayer();
             player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 14 * 20, 2));
             player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 14 * 20, 0));
@@ -57,7 +55,7 @@ public final class beer extends JavaPlugin implements Listener {
         potionMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
         beer.setItemMeta(potionMeta);
         ItemMeta meta = beer.getItemMeta();
-        meta.setDisplayName("Cerveja");
+        meta.setDisplayName("Beer");
         beer.setItemMeta(meta);
         return beer;
     }
